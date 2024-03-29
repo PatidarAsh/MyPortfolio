a8c58758de6d7c7e8ae2fd70bccd5a4aab9cc2e6
PatidarAsh <113142513+PatidarAsh@users.noreply.github.com>
2024-03-29 11:13:48 -0400

    updated UX portfolio

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -46,9 +46,9 @@ const Project = () => {
           <a href="https://www.figma.com/proto/gtRe3osceIdUf9ueCqArGE/Ashvini_Patidar?type=design&node-id=685-412&viewport=1748%2C2031%2C0.39&t=b76cJn1lTgRDykzM-0&scaling=min-zoom&starting-point-node-id=689%3A618" target="_blank" rel="noreferrer">
             <button class="rounded-sm text-white bg-[#C3073F] px-3 py-1 my-3 mx-1 flex items-center hover:bg-[#1e3751]">Code</button>
           </a>
-          <a href="https://www.figma.com/proto/gtRe3osceIdUf9ueCqArGE/Ashvini_Patidar?type=design&node-id=573-1345&viewport=-545%2C-1113%2C0.23&t=XYEr4rCvJC4pbIgb-0&scaling=scale-down&starting-point-node-id=574%3A1351" target="_blank" rel="noreferrer">
+          {/* <a href="https://www.figma.com/proto/gtRe3osceIdUf9ueCqArGE/Ashvini_Patidar?type=design&node-id=573-1345&viewport=-545%2C-1113%2C0.23&t=XYEr4rCvJC4pbIgb-0&scaling=scale-down&starting-point-node-id=574%3A1351" target="_blank" rel="noreferrer">
             <button class="rounded-sm text-white bg-[#C3073F] px-3 py-1 my-3 mx-1 flex items-center hover:bg-[#1e3751]">Demo</button>
-          </a>
+          </a> */}
         </div>
       </div>
       <div class="shadow-md shadow-[#b1b8c1] bg-white group container rounded-md  flex flex-col justify-between text-center items-center mx-auto py-2 px-4 overflow-auto">
